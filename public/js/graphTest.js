b55e227f22dbea15d889e801794dd118e0a90bd7
geert <geert.vandamme@darling.be>
2023-05-12 10:33:03 +0200

    import showGraph function (client side)

---
+++ public/js/graphTest.js
@@ -0,0 +1,23 @@
+function showGraph(evt){
+    let clickedData = getData(evt.target)
+    if (clickedData){
+        let month = clickedData.month
+        let year = clickedData.year
+        let data = report.data["main"].data.filter((d)=>{return (d.d.getMonth() == (month-1) && d.d.getFullYear() == year)})
+        let graph = Plot.plot({
+            marks: [
+                ()=>{let style =  document.createElement("style")
+                    style.appendChild(document.createTextNode(' .info { stroke: "red"} '))
+                    return style
+                },
+                Plot.line(data, {x: "d", y: "posts"}),
+                Plot.circle(data, {x: "d", y: "posts",stroke:"red",r:2,class:"info" })
+            ],
+            y: {grid: true}
+        })
+        const div = document.querySelector("#dataset_detail");
+        div.replaceChild(graph,div.firstChild )
+    }
+}
+
+export {showGraph}
